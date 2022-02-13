3349b5ceca21c18846573c0451853cca83dcef1d
BlackH3art <jaroslaw.musielak@outlook.com>
2022-02-13 15:49:37 +0100

    DB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express();
 const port = 5000;
 
-app.get("/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
 
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+const CONNECTION_URL = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@todo-app.pylyo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+app.get("/", (req, res) => {
+    res.send( "Hello world!" );
 });
+
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => {
+
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+
+    }).catch((err) => {
+        console.error(err);
+    });
