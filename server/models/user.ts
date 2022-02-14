fc820bb14a686778f7561d904454c884a62186c5
Rafal <rgrajcar@gmail.com>
2022-02-14 19:15:19 +0100

    modify pre save  middleware to check if password has changed, add tasks[] to userSchema

--- server/models/user.ts
+++ server/models/user.ts
@@ -22,14 +22,24 @@ const userSchema = new Schema({
         type: String,
         required: true,
         minLength: [5, 'Password must be at least 5 characters!'],
-    }
+    },
+    tasks: [],
 });
 
+/**
+ * funkcja wywoływana przed zapisem do bazy - hashuje wprowadzone hasło
+ * w przypadku gdy nie zostało ono zmienione np. przy edycji profilu użytkownika, funkcja kończy działanie
+ */
 userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
+/**
+ * metoda porównująca wprowadzone hasło z hashem przechowywanym w bazie danych - zwraca boolean
+ */
 userSchema.methods = {
     async checkPassword(password: string): Promise<boolean> {
         return (await bcrypt.compare(password, this.password));
