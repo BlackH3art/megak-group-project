3aec35e9f0198c40958c3c92610335284c8bf7da
LukasP <lukas.straubing@gmail.com>
2022-02-17 15:48:01 +0100

    taskRouter updated

--- server/src/routes/taskRouter.ts
+++ server/src/routes/taskRouter.ts
@@ -9,9 +9,9 @@ taskRouter.post('/:taskId', addTask);
 taskRouter.patch('/:taskId', updateTask);
 taskRouter.delete('/api/:taskId', deleteTask);
 
-// taskRouter.get('/api/user', getUser)
-// taskRouter.post('/api/user/:id', editUser)
-// taskRouter.delete('/api/user/:id', deleteUser)
-// taskRouter.post('/api/user', logOut)
+// taskRouter.get('/user', getUser)
+// taskRouter.post('/user/:id', editUser)
+// taskRouter.delete('/user/:id', deleteUser)
+// taskRouter.post('/user', logOut)
 
 export default taskRouter;
